67b09a3200dd877085784bce67dbb1d40cdf02d3
AbhishekTijare <98269440+AbhishekTijare@users.noreply.github.com>
2022-07-06 14:34:58 +0530

    api using mongoDB

---
+++ src/restaurant/routes/restaurant.js
@@ -0,0 +1,15 @@
+// Routing of express.
+const express = require("express");
+const controller = require("../controllers/restaurant");
+
+// Create router.
+const router = express.Router();
+
+router.post("/", controller.add);
+router.get("/", controller.get);
+router.get("/location/:city", controller.getByLocation);
+router.put("/",controller.update);
+router.delete("/:id",controller.delete);
+router.get("/filter",controller.filter);
+
+module.exports = router;
